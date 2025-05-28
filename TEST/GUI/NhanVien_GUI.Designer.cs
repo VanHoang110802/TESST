76202457bbeaa3f0dba34fb4b0558c0cefa82a96
VanHoang110802 <tvh110802@gmail.com>
2025-05-28 13:45:53 +0700

    Add project files.

---
+++ TEST/GUI/NhanVien_GUI.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace TEST.GUI
+{
+    partial class NhanVien_GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_NhapLuong = new System.Windows.Forms.TextBox();
+            this.label_Luong = new System.Windows.Forms.Label();
+            this.radioButton_ChonNu = new System.Windows.Forms.RadioButton();
+            this.radioButton_ChonNam = new System.Windows.Forms.RadioButton();
+            this.label_GioiTinh = new System.Windows.Forms.Label();
+            this.dateTimePicker_ChonNamSinh = new System.Windows.Forms.DateTimePicker();
+            this.textBox_NhapHoTen = new System.Windows.Forms.TextBox();
+            this.button_TimKiem = new System.Windows.Forms.Button();
+            this.button_Sua = new System.Windows.Forms.Button();
+            this.button_Xoa = new System.Windows.Forms.Button();
+            this.button_Them = new System.Windows.Forms.Button();
+            this.label_NgaySinh = new System.Windows.Forms.Label();
+            this.groupBox_ChucNangChinh = new System.Windows.Forms.GroupBox();
+            this.button_Thoat = new System.Windows.Forms.Button();
+            this.label_HoTen = new System.Windows.Forms.Label();
+            this.groupBox_ThongTinChung = new System.Windows.Forms.GroupBox();
+            this.dataGridView_ThongTin = new System.Windows.Forms.DataGridView();
+            this.groupBox_NhapThongTin = new System.Windows.Forms.GroupBox();
+            this.groupBox_ChucNangChinh.SuspendLayout();
+            this.groupBox_ThongTinChung.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_ThongTin)).BeginInit();
+            this.groupBox_NhapThongTin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_NhapLuong
+            // 
+            this.textBox_NhapLuong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_NhapLuong.Location = new System.Drawing.Point(469, 76);
+            this.textBox_NhapLuong.Name = "textBox_NhapLuong";
+            this.textBox_NhapLuong.Size = new System.Drawing.Size(249, 26);
+            this.textBox_NhapLuong.TabIndex = 8;
+            // 
+            // label_Luong
+            // 
+            this.label_Luong.AutoSize = true;
+            this.label_Luong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Luong.Location = new System.Drawing.Point(393, 80);
+            this.label_Luong.Name = "label_Luong";
+            this.label_Luong.Size = new System.Drawing.Size(49, 19);
+            this.label_Luong.TabIndex = 7;
+            this.label_Luong.Text = "Lương";
+            // 
+            // radioButton_ChonNu
+            // 
+            this.radioButton_ChonNu.AutoSize = true;
+            this.radioButton_ChonNu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton_ChonNu.Location = new System.Drawing.Point(545, 37);
+            this.radioButton_ChonNu.Name = "radioButton_ChonNu";
+            this.radioButton_ChonNu.Size = new System.Drawing.Size(48, 23);
+            this.radioButton_ChonNu.TabIndex = 6;
+            this.radioButton_ChonNu.TabStop = true;
+            this.radioButton_ChonNu.Text = "Nữ";
+            this.radioButton_ChonNu.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_ChonNam
+            // 
+            this.radioButton_ChonNam.AutoSize = true;
+            this.radioButton_ChonNam.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton_ChonNam.Location = new System.Drawing.Point(469, 37);
+            this.radioButton_ChonNam.Name = "radioButton_ChonNam";
+            this.radioButton_ChonNam.Size = new System.Drawing.Size(57, 23);
+            this.radioButton_ChonNam.TabIndex = 5;
+            this.radioButton_ChonNam.TabStop = true;
+            this.radioButton_ChonNam.Text = "Nam";
+            this.radioButton_ChonNam.UseVisualStyleBackColor = true;
+            // 
+            // label_GioiTinh
+            // 
+            this.label_GioiTinh.AutoSize = true;
+            this.label_GioiTinh.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_GioiTinh.Location = new System.Drawing.Point(393, 39);
+            this.label_GioiTinh.Name = "label_GioiTinh";
+            this.label_GioiTinh.Size = new System.Drawing.Size(59, 19);
+            this.label_GioiTinh.TabIndex = 4;
+            this.label_GioiTinh.Text = "Giới tính";
+            // 
+            // dateTimePicker_ChonNamSinh
+            // 
+            this.dateTimePicker_ChonNamSinh.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker_ChonNamSinh.Location = new System.Drawing.Point(96, 76);
+            this.dateTimePicker_ChonNamSinh.Name = "dateTimePicker_ChonNamSinh";
+            this.dateTimePicker_ChonNamSinh.Size = new System.Drawing.Size(255, 26);
+            this.dateTimePicker_ChonNamSinh.TabIndex = 3;
+            // 
+            // textBox_NhapHoTen
+            // 
+            this.textBox_NhapHoTen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_NhapHoTen.Location = new System.Drawing.Point(94, 34);
+            this.textBox_NhapHoTen.Name = "textBox_NhapHoTen";
+            this.textBox_NhapHoTen.Size = new System.Drawing.Size(257, 26);
+            this.textBox_NhapHoTen.TabIndex = 2;
+            // 
+            // button_TimKiem
+            // 
+            this.button_TimKiem.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_TimKiem.Location = new System.Drawing.Point(501, 28);
+            this.button_TimKiem.Name = "button_TimKiem";
+            this.button_TimKiem.Size = new System.Drawing.Size(92, 38);
+            this.button_TimKiem.TabIndex = 3;
+            this.button_TimKiem.Text = "Tìm kiếm";
+            this.button_TimKiem.UseVisualStyleBackColor = true;
+            this.button_TimKiem.Click += new System.EventHandler(this.button_TimKiem_Click);
+            // 
+            // button_Sua
+            // 
+            this.button_Sua.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Sua.Location = new System.Drawing.Point(350, 28);
+            this.button_Sua.Name = "button_Sua";
+            this.button_Sua.Size = new System.Drawing.Size(92, 38);
+            this.button_Sua.TabIndex = 2;
+            this.button_Sua.Text = "Sửa";
+            this.button_Sua.UseVisualStyleBackColor = true;
+            this.button_Sua.Click += new System.EventHandler(this.button_Sua_Click);
+            // 
+            // button_Xoa
+            // 
+            this.button_Xoa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Xoa.Location = new System.Drawing.Point(194, 28);
+            this.button_Xoa.Name = "button_Xoa";
+            this.button_Xoa.Size = new System.Drawing.Size(92, 38);
+            this.button_Xoa.TabIndex = 1;
+            this.button_Xoa.Text = "Xóa";
+            this.button_Xoa.UseVisualStyleBackColor = true;
+            this.button_Xoa.Click += new System.EventHandler(this.button_Xoa_Click);
+            // 
+            // button_Them
+            // 
+            this.button_Them.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Them.Location = new System.Drawing.Point(40, 28);
+            this.button_Them.Name = "button_Them";
+            this.button_Them.Size = new System.Drawing.Size(92, 38);
+            this.button_Them.TabIndex = 0;
+            this.button_Them.Text = "Thêm";
+            this.button_Them.UseVisualStyleBackColor = true;
+            this.button_Them.Click += new System.EventHandler(this.button_Them_Click);
+            // 
+            // label_NgaySinh
+            // 
+            this.label_NgaySinh.AutoSize = true;
+            this.label_NgaySinh.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_NgaySinh.Location = new System.Drawing.Point(21, 76);
+            this.label_NgaySinh.Name = "label_NgaySinh";
+            this.label_NgaySinh.Size = new System.Drawing.Size(69, 19);
+            this.label_NgaySinh.TabIndex = 1;
+            this.label_NgaySinh.Text = "Ngày sinh";
+            // 
+            // groupBox_ChucNangChinh
+            // 
+            this.groupBox_ChucNangChinh.Controls.Add(this.button_Thoat);
+            this.groupBox_ChucNangChinh.Controls.Add(this.button_TimKiem);
+            this.groupBox_ChucNangChinh.Controls.Add(this.button_Sua);
+            this.groupBox_ChucNangChinh.Controls.Add(this.button_Xoa);
+            this.groupBox_ChucNangChinh.Controls.Add(this.button_Them);
+            this.groupBox_ChucNangChinh.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox_ChucNangChinh.Location = new System.Drawing.Point(12, 173);
+            this.groupBox_ChucNangChinh.Name = "groupBox_ChucNangChinh";
+            this.groupBox_ChucNangChinh.Size = new System.Drawing.Size(776, 81);
+            this.groupBox_ChucNangChinh.TabIndex = 4;
+            this.groupBox_ChucNangChinh.TabStop = false;
+            this.groupBox_ChucNangChinh.Text = "Chức năng chính";
+            // 
+            // button_Thoat
+            // 
+            this.button_Thoat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Thoat.Location = new System.Drawing.Point(649, 28);
+            this.button_Thoat.Name = "button_Thoat";
+            this.button_Thoat.Size = new System.Drawing.Size(92, 38);
+            this.button_Thoat.TabIndex = 4;
+            this.button_Thoat.Text = "Thoát";
+            this.button_Thoat.UseVisualStyleBackColor = true;
+            this.button_Thoat.Click += new System.EventHandler(this.button_Thoat_Click);
+            // 
+            // label_HoTen
+            // 
+            this.label_HoTen.AutoSize = true;
+            this.label_HoTen.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_HoTen.Location = new System.Drawing.Point(21, 39);
+            this.label_HoTen.Name = "label_HoTen";
+            this.label_HoTen.Size = new System.Drawing.Size(50, 19);
+            this.label_HoTen.TabIndex = 0;
+            this.label_HoTen.Text = "Họ tên";
+            // 
+            // groupBox_ThongTinChung
+            // 
+            this.groupBox_ThongTinChung.Controls.Add(this.dataGridView_ThongTin);
+            this.groupBox_ThongTinChung.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox_ThongTinChung.Location = new System.Drawing.Point(12, 291);
+            this.groupBox_ThongTinChung.Name = "groupBox_ThongTinChung";
+            this.groupBox_ThongTinChung.Size = new System.Drawing.Size(776, 222);
+            this.groupBox_ThongTinChung.TabIndex = 5;
+            this.groupBox_ThongTinChung.TabStop = false;
+            this.groupBox_ThongTinChung.Text = "Thông tin chung";
+            // 
+            // dataGridView_ThongTin
+            // 
+            this.dataGridView_ThongTin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_ThongTin.Location = new System.Drawing.Point(6, 28);
+            this.dataGridView_ThongTin.Name = "dataGridView_ThongTin";
+            this.dataGridView_ThongTin.Size = new System.Drawing.Size(764, 188);
+            this.dataGridView_ThongTin.TabIndex = 0;
+            // 
+            // groupBox_NhapThongTin
+            // 
+            this.groupBox_NhapThongTin.Controls.Add(this.textBox_NhapLuong);
+            this.groupBox_NhapThongTin.Controls.Add(this.label_Luong);
+            this.groupBox_NhapThongTin.Controls.Add(this.radioButton_ChonNu);
+            this.groupBox_NhapThongTin.Controls.Add(this.radioButton_ChonNam);
+            this.groupBox_NhapThongTin.Controls.Add(this.label_GioiTinh);
+            this.groupBox_NhapThongTin.Controls.Add(this.dateTimePicker_ChonNamSinh);
+            this.groupBox_NhapThongTin.Controls.Add(this.textBox_NhapHoTen);
+            this.groupBox_NhapThongTin.Controls.Add(this.label_NgaySinh);
+            this.groupBox_NhapThongTin.Controls.Add(this.label_HoTen);
+            this.groupBox_NhapThongTin.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox_NhapThongTin.Location = new System.Drawing.Point(12, 12);
+            this.groupBox_NhapThongTin.Name = "groupBox_NhapThongTin";
+            this.groupBox_NhapThongTin.Size = new System.Drawing.Size(776, 126);
+            this.groupBox_NhapThongTin.TabIndex = 3;
+            this.groupBox_NhapThongTin.TabStop = false;
+            this.groupBox_NhapThongTin.Text = "Nhập thông tin";
+            // 
+            // NhanVien_GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 524);
+            this.Controls.Add(this.groupBox_ChucNangChinh);
+            this.Controls.Add(this.groupBox_ThongTinChung);
+            this.Controls.Add(this.groupBox_NhapThongTin);
+            this.Name = "NhanVien_GUI";
+            this.Text = "NhanVien_GUI";
+            this.Load += new System.EventHandler(this.NhanVien_GUI_Load);
+            this.groupBox_ChucNangChinh.ResumeLayout(false);
+            this.groupBox_ThongTinChung.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_ThongTin)).EndInit();
+            this.groupBox_NhapThongTin.ResumeLayout(false);
+            this.groupBox_NhapThongTin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox_NhapLuong;
+        private System.Windows.Forms.Label label_Luong;
+        private System.Windows.Forms.RadioButton radioButton_ChonNu;
+        private System.Windows.Forms.RadioButton radioButton_ChonNam;
+        private System.Windows.Forms.Label label_GioiTinh;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_ChonNamSinh;
+        private System.Windows.Forms.TextBox textBox_NhapHoTen;
+        private System.Windows.Forms.Button button_TimKiem;
+        private System.Windows.Forms.Button button_Sua;
+        private System.Windows.Forms.Button button_Xoa;
+        private System.Windows.Forms.Button button_Them;
+        private System.Windows.Forms.Label label_NgaySinh;
+        private System.Windows.Forms.GroupBox groupBox_ChucNangChinh;
+        private System.Windows.Forms.Button button_Thoat;
+        private System.Windows.Forms.Label label_HoTen;
+        private System.Windows.Forms.GroupBox groupBox_ThongTinChung;
+        private System.Windows.Forms.GroupBox groupBox_NhapThongTin;
+        private System.Windows.Forms.DataGridView dataGridView_ThongTin;
+    }
+}
---
+++ TEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("0866998885")]
+[assembly: AssemblyProduct("TEST")]
+[assembly: AssemblyCopyright("Copyright © 0866998885 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("528e5061-13c7-427d-bd45-6b06d631d0f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
